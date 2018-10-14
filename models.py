c267bbe7b596bc0b67cd7ceca0218d13ca86de6c
root <rafaelszp.info@gmail.com>
2018-10-14 03:03:48 +0000

    chg

--- models.py
+++ models.py
@@ -19,69 +19,44 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        #self.conv1 = nn.Conv2d(1, 32, 5)
-        
-        #self.layer1 = nn.Sequential(
-        #    nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
-        #    nn.BatchNorm2d(16),
-        #    nn.ReLU(),
-        #    nn.MaxPool2d(kernel_size=2, stride=2))
-        
+                
         #input hxw = 224
-        self.conv1 = nn.Sequential(
+        self.convnet = nn.Sequential(
+            #conv1
             nn.Conv2d(1,32,kernel_size=5,stride=1),
-            nn.Relu(),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2,stride=2),
-            nn.Dropout(p=0.1)
-        )
-        #out = 110
-        
-        self.conv2 = nn.Sequential(
+            nn.Dropout(p=0.1),#out = 110
+            #conv2
             nn.Conv2d(32,64,kernel_size=3,stride=1),
-            nn.Relu(),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2,stride=2),
-            nn.Dropout(p=0.2)
-        )
-        #out= 54
-        
-        self.conv3 = nn.Sequential(
+            nn.Dropout(p=0.2),#out= 54
+            #conv3
             nn.Conv2d(64,128,kernel_size=3,stride=1),
-            nn.Relu(),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2,stride=2),
-            nn.Dropout(p=0.3)
-        )
-        #out=26
-        
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(128,256,kernel_size=3,stride=1),
-            nn.Relu(),
+            nn.Dropout(p=0.3),#out=26
+            #conv4
+            nn.Conv2d(128,256,kernel_size=2,stride=1),
+            nn.ReLU(),
             nn.MaxPool2d(kernel_size=2,stride=2),
-            nn.Dropout(p=0.4)
+            nn.Dropout(p=0.4),#out=12            
         )
-        #out=12
-        self.conv5 = nn.Sequential(
-            nn.Conv2d(256,512,kernel_size=3,stride=1),
-            nn.Relu(),
-            nn.MaxPool2d(kernel_size=2,stride=2),
-            nn.Dropout(p=0.4)
-        )
-        #out=5
-        
-        self.dense1 = nn.Sequential(
-            nn.Linear(512*5*5,1000),
-            nn.Relu(),
-            nn.Dropout(p=0.5)           
-        )
-        
-        self.dense2 = nn.Sequential(
+        self.densenet = nn.Sequential(
+            #dense1
+            nn.Linear(256*12*12,1000),
+            nn.ReLU(),
+            nn.Dropout(p=0.5) ,
+            #dense2
             nn.Linear(1000,1000),
-            nn.Relu(),
-            nn.Dropout(p=0.6)           
+            nn.ReLU(),
+            nn.Dropout(p=0.6),
+            #dense3
+            nn.Linear(1000,2*68),
         )
-        
-        self.dense3 = nn.Sequential(
-            nn.Linear(1000,2),
-        )
+
+      
         
         
         ## Note that among the layers to add, consider including:
@@ -93,7 +68,9 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
+        x = self.convnet(x)
+        x = x.view(x.size(0), -1)
+        x = self.densenet(x)      
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
